e916a4ef2269cd5f9e5aa5ad7be60fb41da859d9
Giovanna Rocha <silvarochagiovanna@gmail.com>
2023-09-13 19:47:01 -0300

    att

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 //pesquisarCep recebe uma arrow Function
 const pesquisarCep = async () => {
+
+    limparCampos();
+
     const cep = document.getElementById('cep').value;
     //utilizando o template string para concatenar string
     const url = `http://viacep.com.br/ws/${cep}/json/`;
@@ -12,13 +15,16 @@ const pesquisarCep = async () => {
 
         const dados = await fetch(url);
         const endereco = await dados.json();
-        // if(endereco.hasOwnPreperty());
-        preencherFormulario(endereco);
+
+        if (endereco.hasOwnProperty('erro')) {
+            document.getElementById('logradouro').value = 'CEP não localizado';
+        } else {
+            preencherFormulario(endereco);
+        }
+
     } else {
-        document.getElementById('logradouro').value = 'CEP não encontrado!';
+        document.getElementById('logradouro').value = 'Digitação incorreta!';
     }
-    // console.log(endereco);
-    
 }
 
 const preencherFormulario = (endereco) => {
@@ -35,6 +41,13 @@ const preencherFormulario = (endereco) => {
 // o cifrão ($) indica que é o fim da validação;
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const limparCampos = () => {
+    document.getElementById('logradouro').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
 
 document.getElementById('cep').addEventListener('focusout', pesquisarCep);
 //addEventListener "escuta" todos os eventos do controle que definimos pelo id.
